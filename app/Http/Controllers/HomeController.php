b7f3d32df4c2ef62722d20141e6da9258653cc1c
Reynaldi1912 <nexrey19@gmail.com>
2022-07-04 12:03:57 +0700

    bug fixed

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Auth;
 class HomeController extends Controller
 {
     /**
@@ -11,10 +11,10 @@ class HomeController extends Controller
      *
      * @return void
      */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
 
     /**
      * Show the application dashboard.
@@ -23,6 +23,10 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('admin.index');
+        if(Auth::check() == 1){
+            return view('admin.index');
+        }else if(Auth::check() == 0){
+            return view('home');
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/home', function () {
-    return view('home');
-});
+// Route::get('/home', function () {
+//     return view('home');
+// });
+// Route::get('/', function () {
+//     return view('home');
+// });
 
 Route::get('/kalkulator', function () {
     return view('kalkulator');
@@ -36,3 +39,4 @@ Route::prefix("admin")->group(function () {
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
